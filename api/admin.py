7541ce2cec6331d57ae80068af43dad236ebb35f
jaya2320 <jayagupta752@gmail.com>
2021-11-27 13:08:06 +0530

    first commit

---
+++ api/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+@admin.register(Transport)
+class TransportAdmin(admin.ModelAdmin):
+    list_display = ('id','company_name','contact_no','profile_pic','address','availability')
+
+@admin.register(Lab)
+class LabAdmin(admin.ModelAdmin):
+    list_display = ('id','location','lab_name','contact_info','charges','test_name','testing_at')
+
+@admin.register(Insuarance)
+class CompanyRegisterAdmin(admin.ModelAdmin):
+    list_display = ('id','company_name','company_code','contact_no','email','address')
---
+++ api/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Transport(models.Model):
+    company_name = models.CharField(max_length =50)
+    contact_no = models.CharField(max_length =10)
+    profile_pic =  models.ImageField(upload_to='transport/profile_image/',default='default.ico')
+    address = models.CharField(max_length =50)
+    availability = models.CharField(max_length =50)
+
+class Lab(models.Model):
+    lab_options = (
+        ('PLACE','PLACE'),
+        ('LAB', 'LAB'),
+    )
+    location = models.CharField(max_length =50)
+    lab_name = models.CharField(max_length =50)
+    contact_info = models.CharField(max_length =10)
+    charges = models.IntegerField()
+    test_name = models.CharField(max_length =50)
+    testing_at = models.CharField(
+        max_length=10, choices=lab_options, default="LAB")
+
+class Insuarance(models.Model):
+    company_name = models.CharField(max_length =50)
+    company_code = models.CharField(max_length =50)
+    contact_no = models.CharField(max_length =50)
+    email = models.EmailField(max_length =50)
+    address = models.CharField(max_length =50)
+
+
